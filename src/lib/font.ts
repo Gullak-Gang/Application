bf53544f0ac42bf483953846fd8f4f777ffeab2a
Drish-xD <drish.xd@gmail.com>
2024-11-29 12:07:39 +0530

    init repo

---
+++ src/lib/font.ts
@@ -0,0 +1,18 @@
+import { JetBrains_Mono, Poppins } from "next/font/google";
+import { cn } from "./utils";
+
+const poppins = Poppins({
+  display: "swap",
+  variable: "--font-sans",
+  weight: ["400", "600", "800"],
+});
+
+const fontMain = JetBrains_Mono({
+  weight: "variable",
+  variable: "--font-mono",
+  fallback: ["system-ui", "monospace"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const fonts = cn(fontMain.variable, poppins.variable);
