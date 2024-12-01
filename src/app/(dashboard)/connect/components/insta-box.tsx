d69810b6d9ff222147c3ea691745bd432ac78812
Tanish2002 <tanishkhare@gmail.com>
2024-12-01 19:36:36 +0530

    add instagram instructions

--- src/app/(dashboard)/connect/components/insta-box.tsx
+++ src/app/(dashboard)/connect/components/insta-box.tsx
@@ -12,6 +12,19 @@ const InstaConnectBox = () => {
     <MagicCard className="flex justify-center items-center w-full aspect-video">
       <form action={saveToDB} className="flex flex-col space-y-4">
         <Input type="text" name="access_token" placeholder="Access Token" className="w-3/4" required />
+
+        {/* Instructions */}
+        <div className="flex justify-between items-center">
+          <span className="text-sm text-gray-500">Need help?</span>
+          <button
+            type="button"
+            onClick={() => alert("To get your Instagram access token:\n1. Create an account at https://apify.com\n2. Go to https://console.apify.com/settings/integrations\n3. Generate an API token")}
+            className="text-blue-600 hover:text-blue-800"
+          >
+            Get instructions
+          </button>
+        </div>
+
         <Input type="text" name="hashtag" placeholder="Hashtag (e.g., #nature)" className="w-3/4" required />
         <Input
           type="number"
