3cbaf29baf1e81e2b2bf3a18718eb4f9f70e566e
Drish-xD <drish.xd@gmail.com>
2024-12-01 20:24:47 +0530

    fix: package error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/dashboard")
   return (
     <div className="flex justify-center items-center">
       <Button>Hello World</Button>
